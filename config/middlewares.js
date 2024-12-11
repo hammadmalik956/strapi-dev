a107864fdcb81f4f9061113c9ffece8dfb1afcd3
Malik Hammad Hameed <hammadhameed956@gmail.com>
2024-12-11 14:59:35 +0500

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,37 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    module.exports = [
+      'strapi::errors',
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'stag-essaysasia-strapi-image-assests-buck.s3.us-west-2.amazonaws.com', // change here
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'stag-essaysasia-strapi-image-assests-buck.s3.us-west-2.amazonaws.com', // change here
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
